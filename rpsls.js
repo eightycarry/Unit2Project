8a1279ebe68312ca7d1c8063717f47942666bdd2
EightyCarry <calvin_lor@yahoo.com>
2020-12-06 11:00:30 -0600

    Rock Paper Scissors Lizard Spock

--- rpsls.js
+++ rpsls.js
@@ -19,29 +19,29 @@ $(document).ready(function(){
         var aiChoice = Math.floor(Math.random() * 5);
 
         if (aiChoice === 0) {//ai chooses rock
-            $("#playerChoice").text("Rock")
-            $("#aiChoice").text("Rock")
+            $("#playerChoice").text("Rock");
+            $("#aiChoice").text("Rock");
             $("#results").text("TIE");
             playerScore++;
             aiScore++;
         } else if (aiChoice === 1) { //ai chooses paper
-            $("#playerChoice").text("Rock")
-            $("#aiChoice").text("Paper")
+            $("#playerChoice").text("Rock");
+            $("#aiChoice").text("Paper");
             $("#results").text("AI WINS");
             aiScore++;
         } else if (aiChoice === 2) { //ai chooses scissors
-            $("#playerChoice").text("Rock")
-            $("#aiChoice").text("Scissors")
+            $("#playerChoice").text("Rock");
+            $("#aiChoice").text("Scissors");
             $("#results").text("PLAYER WINS");
             playerScore++;
         } else if (aiChoice === 3) {//ai chooses lizard
-            $("#playerChoice").text("Rock")
-            $("#aiChoice").text("Lizard")
+            $("#playerChoice").text("Rock");
+            $("#aiChoice").text("Lizard");
             $("#results").text("PLAYER WINS");
             playerScore++;
         } else {//ai chooses spock
-            $("#playerChoice").text("Rock")
-            $("#aiChoice").text("Spock")
+            $("#playerChoice").text("Rock");
+            $("#aiChoice").text("Spock");
             $("#results").text("AI WINS");
             aiScore++;
         }
@@ -60,29 +60,29 @@ $(document).ready(function(){
         var aiChoice = Math.floor(Math.random() * 5);
 
         if (aiChoice === 0) {//ai chooses rock
-            $("#playerChoice").text("Paper")
-            $("#aiChoice").text("Rock")
+            $("#playerChoice").text("Paper");
+            $("#aiChoice").text("Rock");
             $("#results").text("PLAYER WINS");
             playerScore++;
         } else if (aiChoice === 1) { //ai chooses paper
-            $("#playerChoice").text("Paper")
-            $("#aiChoice").text("Paper")
+            $("#playerChoice").text("Paper");
+            $("#aiChoice").text("Paper");
             $("#results").text("TIE");
             playerScore++;
             aiScore++;
         } else if (aiChoice === 2) { //ai chooses scissors
-            $("#playerChoice").text("Paper")
-            $("#aiChoice").text("Scissors")
+            $("#playerChoice").text("Paper");
+            $("#aiChoice").text("Scissors");
             $("#results").text("AI WINS");
             aiScore++;
         } else if (aiChoice === 3) {//ai chooses lizard
-            $("#playerChoice").text("Paper")
-            $("#aiChoice").text("Lizard")
+            $("#playerChoice").text("Paper");
+            $("#aiChoice").text("Lizard");
             $("#results").text("AI WINS");
             aiScore++;
         } else {//ai chooses spock
-            $("#playerChoice").text("Paper")
-            $("#aiChoice").text("Spock")
+            $("#playerChoice").text("Paper");
+            $("#aiChoice").text("Spock");
             $("#results").text("PLAYER WINS");
             playerScore++;
         }
@@ -100,29 +100,29 @@ $(document).ready(function(){
         var aiChoice = Math.floor(Math.random() * 5);
 
         if (aiChoice === 0) {//ai chooses rock
-            $("#playerChoice").text("Scissors")
-            $("#aiChoice").text("Rock")
+            $("#playerChoice").text("Scissors");
+            $("#aiChoice").text("Rock");
             $("#results").text("AI WINS");
             aiScore++;
         } else if (aiChoice === 1) { //ai chooses paper
-            $("#playerChoice").text("Scissors")
-            $("#aiChoice").text("Paper")
+            $("#playerChoice").text("Scissors");
+            $("#aiChoice").text("Paper");
             $("#results").text("PLAYER WINS");
             playerScore++;
         } else if (aiChoice === 2) { //ai chooses scissors
-            $("#playerChoice").text("Scissors")
-            $("#aiChoice").text("Scissors")
+            $("#playerChoice").text("Scissors");
+            $("#aiChoice").text("Scissors");
             $("#results").text("TIE");
             playerScore++;
             aiScore++;
         } else if (aiChoice === 3) {//ai chooses lizard
-            $("#playerChoice").text("Scissors")
-            $("#aiChoice").text("Lizard")
+            $("#playerChoice").text("Scissors");
+            $("#aiChoice").text("Lizard");
             $("#results").text("PLAYER WINS");
             playerScore++;
         } else {//ai chooses spock
-            $("#playerChoice").text("Scissors")
-            $("#aiChoice").text("Spock")
+            $("#playerChoice").text("Scissors");
+            $("#aiChoice").text("Spock");
             $("#results").text("AI WINS");
             aiScore++;
         }
@@ -140,29 +140,29 @@ $(document).ready(function(){
         var aiChoice = Math.floor(Math.random() * 5);
 
         if (aiChoice === 0) {//ai chooses rock
-            $("#playerChoice").text("Lizard")
-            $("#aiChoice").text("Rock")
+            $("#playerChoice").text("Lizard");
+            $("#aiChoice").text("Rock");
             $("#results").text("AI WINS");
             aiScore++;
         } else if (aiChoice === 1) { //ai chooses paper
-            $("#playerChoice").text("Lizard")
-            $("#aiChoice").text("Paper")
+            $("#playerChoice").text("Lizard");
+            $("#aiChoice").text("Paper");
             $("#results").text("PLAYER WINS");
             playerScore++;
         } else if (aiChoice === 2) { //ai chooses scissors
-            $("#playerChoice").text("Lizard")
-            $("#aiChoice").text("Scissors")
+            $("#playerChoice").text("Lizard");
+            $("#aiChoice").text("Scissors");
             $("#results").text("AI WINS");
             aiScore++;
         } else if (aiChoice === 3) {//ai chooses lizard
-            $("#playerChoice").text("Lizard")
-            $("#aiChoice").text("Lizard")
+            $("#playerChoice").text("Lizard");
+            $("#aiChoice").text("Lizard");
             $("#results").text("TIE");
             playerScore++;
             aiScore++;
         } else {//ai chooses spock
-            $("#playerChoice").text("Lizard")
-            $("#aiChoice").text("Spock")
+            $("#playerChoice").text("Lizard");
+            $("#aiChoice").text("Spock");
             $("#results").text("PLAYER WINS");
             playerScore++;
         }
@@ -180,28 +180,28 @@ $(document).ready(function(){
         var aiChoice = Math.floor(Math.random() * 5);
 
         if (aiChoice === 0) {//ai chooses rock
-            $("#playerChoice").text("Spock")
-            $("#aiChoice").text("Rock")
+            $("#playerChoice").text("Spock");
+            $("#aiChoice").text("Rock");
             $("#results").text("PLAYER WINS");
             playerScore++;
         } else if (aiChoice === 1) { //ai chooses paper
-            $("#playerChoice").text("Spock")
-            $("#aiChoice").text("Paper")
+            $("#playerChoice").text("Spock");
+            $("#aiChoice").text("Paper");
             $("#results").text("AI WINS");
             aiScore++;
         } else if (aiChoice === 2) { //ai chooses scissors
-            $("#playerChoice").text("Spock")
-            $("#aiChoice").text("Scissors")
+            $("#playerChoice").text("Spock");
+            $("#aiChoice").text("Scissors");
             $("#results").text("PLAYER WINS");
             playerScore++;
         } else if (aiChoice === 3) {//ai chooses lizard
-            $("#playerChoice").text("Spock")
-            $("#aiChoice").text("Lizard")
+            $("#playerChoice").text("Spock");
+            $("#aiChoice").text("Lizard");
             $("#results").text("AI WINS");
             aiScore++;
         } else {//ai chooses spock
-            $("#playerChoice").text("Spock")
-            $("#aiChoice").text("Spock")
+            $("#playerChoice").text("Spock");
+            $("#aiChoice").text("Spock");
             $("#results").text("TIE");
             playerScore++;
             aiScore++;
